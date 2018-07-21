f1915ac7dab29da8a87aa20ae8afde7ca1e6380f
Nissen <handeltonido@gmail.com>
2018-07-22 02:31:36 +0800

    實現和firebase的連結

--- javascript/vue.js
+++ javascript/vue.js
@@ -1,14 +1,25 @@
+var config = {
+    apiKey: "AIzaSyCYwii4dJlgVvehy3wQpS1Aw8iaXZmNrO8",
+    authDomain: "todo-project-e731b.firebaseapp.com",
+    databaseURL: "https://todo-project-e731b.firebaseio.com",
+    projectId: "todo-project-e731b",
+    storageBucket: "todo-project-e731b.appspot.com",
+    messagingSenderId: "803994604460"
+};
+firebase.initializeApp(config);
+
+// var refName = firebase.database().ref().set({})
+// 清除資料庫
+
+var todos = [];
+
+
 var app = new Vue({
     el: "#app",
     data: {
         newTodo: "",
         catagory_selected: "",
-        todos: [{
-            id: '123',
-            title: '任務',
-            catagory: '',
-            completed: false
-        }],
+        todos: todos,
         visibilty: "all"
     },
     methods: {
@@ -29,18 +40,31 @@ var app = new Vue({
                 catagory: value_catagory,
                 completed: false
             });
+            firebase.database().ref().set(this.todos);
             this.newTodo = '';
         },
         removeTodo: function (key) {
             this.todos.splice(key, 1);
+            firebase.database().ref().set(this.todos);
         },
+
+    },
+    created: function () {
+        // firebase.database().ref();
+        // firebase.database().ref().set(todos);
+        // 把資料推入資料庫
+        var vm = this;
+        var refname = firebase.database().ref()
+        refname.once("value", function (snapshot) {
+            vm.todos = snapshot.val()
+
+        })
 
     },
     computed: {
         filteredTodo: function () {
-
-
             if (this.visibilty == 'all') {
+
                 return this.todos;
             } else if (this.visibilty == 'life-main-line') {
                 var newTodos = [];
@@ -49,6 +73,7 @@ var app = new Vue({
                         newTodos.push(item);
                     }
                 });
+
                 return newTodos;
             } else if (this.visibilty == 'life-vice-line') {
                 var newTodos = [];
